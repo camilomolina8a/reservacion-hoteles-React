479d9034bc69dbbc8ae01f906927eda97cd772a6
Camilo Molina <camilomolina8a@gmail.com>
2021-08-11 17:29:40 -0500

    3. Header listo con los manejos de estado (OJO: los estados estan locales en el Header)

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import './styles/App.css'
 import Header from './components/Header'
 
 export default function  App () {
+
+
+  /*                    
+      
+  
+  
+  
+  
+  
+  
+  */
+
+
   return (
     <div className="app-container">
-      <Header/>
+      <Header />
     </div>
   )
 } 
